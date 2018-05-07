aa972d7806a4351ecb779a34a76f5e56d84c434b
Sebastian Murawczik <sebastianmurawczik@MacBook-Pro-de-Sebastian.local>
2018-05-07 20:55:53 -0300

    adding texts for app first pages

--- src/pages/initial-slider/initial-slider.ts
+++ src/pages/initial-slider/initial-slider.ts
@@ -16,17 +16,21 @@ export class InitialSliderPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public tracker: AnalyticsProvider) {
     this.slidesData = [
-      // {
-      //   title: "Guia 1",
-      //   description: "Esta es la pagina 1 de guia, la podras volver a ver luego",
-      // },
-      // {
-      //   title: "Guia 2",
-      //   description: "Esta es la 2da pagina de la guia y tambien la podras volver a ver luego",
-      // },
+      {
+        title: "Guia 1",
+        description: "La App de Wikigrado está pensada para que a través de los vídeos, puedas conocer toda la oferta de grados y universidades de España.",
+      },
+      {
+        title: "Guia 2",
+        description: "Con videos superdinamicos, podrás conocer en que consiste cada grado y además, verás ejemplos reales con sus salidas laborales.",
+      },
       {
         title: "Guia 3",
-        description: "Esta es la ultima pagina de la guia, luego viene el registro",
+        description: "Registrándote, tendrás tu historial, favoritos, avatar y mas...",
+      },
+      {
+        title: "Guia 4",
+        description: "Podrás hacer una búsqueda muy precisa tanto de las universidades, como de los grados que se imparten en cada una. Bienvenido a Wikigrado.<br/><br/> Comenzamos? Hay un test de orientación, esperándote...",
       }
     ];
   }
